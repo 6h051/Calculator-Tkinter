a298e9babc6a546fb87b6333a6ce2b9130c736dc
6h051 <nicosavemind1@protonmail.com>
2022-04-03 22:03:25 -0500

    Add first buttons

---
+++ suma.py
@@ -0,0 +1,28 @@
+from cgitb import text
+from tkinter import *
+root = Tk()
+
+l = Entry()
+
+
+
+l.pack()
+
+
+
+one_button = Button(text="1")
+
+one_button.pack()
+
+
+two_button = Button(text="2")
+two_button.pack()
+
+three_button = Button(text="3")
+three_button.pack()
+four_button = Button(text="4")
+four_button.pack()
+
+
+
+l.mainloop()
